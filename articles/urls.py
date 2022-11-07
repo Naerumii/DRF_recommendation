1462a1f356d8985a51ecd3176f84fdde4b0105b9
naerumii <github>
2022-11-08 02:44:54 +0900

    Fix : slash error

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ from articles import views
 urlpatterns = [
     #댓글 url
     path('<int:article_id>/comment/', views.CommentView.as_view(), name='comment_view'),
-    path('<int:article_id>/comment/<int:comment_id>', views.CommentDetailView.as_view(), name='comment_detail_view'),
+    path('<int:article_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
     
     #demo urls
     path('', views.ArticleView.as_view(), name='article_view'),
